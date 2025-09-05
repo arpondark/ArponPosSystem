e4c132a33a3f6bc10c80a2d475647108b8f33f8b
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-09-06 00:15:16 +0600

    test1

--- src/main/java/com/arpon007/ArponPosSystem/service/impl/CustomerUserImplemantation.java
+++ src/main/java/com/arpon007/ArponPosSystem/service/impl/CustomerUserImplemantation.java
@@ -1,14 +1,29 @@
 package com.arpon007.ArponPosSystem.service.impl;
 
 
+import com.arpon007.ArponPosSystem.Repo.UserRepository;
+import com.arpon007.ArponPosSystem.models.User;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
 public class CustomerUserImplemantation implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepo;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepo.findByEmail(username);
+        if(user == null){
+            throw new UsernameNotFoundException("User Not Found !!!");
+        }
+
+        GrantedAuthority grantedAuthority = new SimpleGrantedAuthority(user.getRole().toString());
+
         return null;
     }
 }
