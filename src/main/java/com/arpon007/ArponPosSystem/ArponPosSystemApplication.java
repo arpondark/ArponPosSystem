d09ceb2d095f1659c7b11d1e4a88e021a894ea27
MD SHAZAMN MAHMUD ARPON <51114057+arpondark@users.noreply.github.com>
2025-09-05 21:52:35 +0600

    first commit

---
+++ src/main/java/com/arpon007/ArponPosSystem/ArponPosSystemApplication.java
@@ -0,0 +1,13 @@
+package com.arpon007.ArponPosSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArponPosSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArponPosSystemApplication.class, args);
+	}
+
+}
